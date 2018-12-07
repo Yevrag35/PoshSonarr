88a561dca69844d0b53de10b7e0bad8629e89934
Mike Garvey <mike.garvey@yevrag35.com>
2018-12-07 16:21:05 -0500

    Working on GetRootFolder

---
+++ Cmdlets/GetSonarrRootFolder.cs
@@ -0,0 +1,21 @@
+﻿using MG.Api;
+using Sonarr.Api.Endpoints;
+using Sonarr.Api.Enums;
+using Sonarr.Api.Results;
+using System;
+using System.Management.Automation;
+
+namespace Sonarr.Api.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "SonarrRootFolder", ConfirmImpact = ConfirmImpact.None)]
+    [CmdletBinding(PositionalBinding = false)]
+    public class GetSonarrRootFolder : BaseCmdlet
+    {
+        protected override void BeginProcessing() => base.BeginProcessing();
+
+        protected override void ProcessRecord()
+        {
+            
+        }
+    }
+}
---
+++ Endpoints/Rootfolder.cs
@@ -0,0 +1,23 @@
+﻿using Sonarr.Api.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Sonarr.Api.Endpoints
+{
+    public class Rootfolder : ISonarrEndpoint
+    {
+        private const string _ep = "/api/rootfolder";
+        public string Value { get; }
+
+        public Uri RelativeEndpoint => new Uri(_ep, UriKind.Relative);
+        public SonarrMethod[] MethodsAllowed => new SonarrMethod[1] { SonarrMethod.GET };
+
+        public Rootfolder() => Value = _ep;
+
+        public static implicit operator string(Rootfolder rf) => rf.Value;
+
+        IEnumerator<string> IEnumerable<string>.GetEnumerator() => new List<string>(1) { this.Value }.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => new List<string>(1) { this.Value }.GetEnumerator();
+    }
+}
