b60dbe6e4ac6803d18743e0bb38799848b83d42f
Mike Garvey <mike.garvey@yevrag35.com>
2019-07-22 20:34:25 -0400

    Starting Commands

---
+++ MG.Sonarr-Core/Cmdlets/Bases/BasePostCommandCmdlet.cs
@@ -0,0 +1,57 @@
+﻿using MG.Sonarr.Results;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace MG.Sonarr.Cmdlets
+{
+    [CmdletBinding(SupportsShouldProcess = true)]
+    public abstract class BasePostCommandCmdlet : BaseSonarrCmdlet
+    {
+        #region FIELDS/CONSTANTS
+        protected private const string BASE_EP = "/command";
+        protected private Dictionary<string, object> parameters;
+
+        protected abstract string Command { get; }
+
+        #endregion
+
+        #region PARAMETERS
+
+
+        #endregion
+
+        #region CMDLET PROCESSING
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            parameters = new Dictionary<string, object>(1)
+            {
+                { "name", this.Command }
+            };
+        }
+
+        protected override void ProcessRecord()
+        {
+            string postBody = JsonConvert.SerializeObject(parameters, Formatting.Indented);
+            string cmdOut = _api.SonarrPost(BASE_EP, postBody);
+            if (!string.IsNullOrEmpty(cmdOut))
+            {
+                CommandOutput cmdOutput = SonarrHttpClient.ConvertToSonarrResult<CommandOutput>(cmdOut);
+                base.WriteObject(cmdOutput);
+            }
+        }
+
+        #endregion
+
+        #region BACKEND METHODS
+
+
+        #endregion
+    }
+}
---
+++ MG.Sonarr-Core/Cmdlets/Commands/InvokeSeriesRefresh.cs
@@ -0,0 +1,52 @@
+﻿using MG.Sonarr.Results;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace MG.Sonarr.Cmdlets
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "SeriesRefresh", ConfirmImpact = ConfirmImpact.High, SupportsShouldProcess = true)]
+    [OutputType(typeof(CommandOutput))]
+    [Alias("Start-SeriesRefresh")]
+    [CmdletBinding(PositionalBinding = false)]
+    public class InvokeSeriesRefresh : BasePostCommandCmdlet
+    {
+        #region FIELDS/CONSTANTS
+        protected override string Command => "RefreshSeries";
+
+        #endregion
+
+        #region PARAMETERS
+        [Parameter(Mandatory = false, Position = 0, ValueFromPipelineByPropertyName = true)]
+        public long SeriesId { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter Force { get; set; }
+
+        #endregion
+
+        #region CMDLET PROCESSING
+        protected override void BeginProcessing() => base.BeginProcessing();
+
+        protected override void ProcessRecord()
+        {
+            string msg = "Refresh all series";
+            if (this.MyInvocation.BoundParameters.ContainsKey("SeriesId"))
+            {
+                parameters.Add("seriesId", this.SeriesId);
+                msg = string.Format("Refresh series id {0}", this.SeriesId);
+            }
+
+            if (this.Force.ToBool() || base.ShouldProcess(msg, "Invoke"))
+            {
+                base.ProcessRecord();
+            }
+        }
+
+        #endregion
+    }
+}
