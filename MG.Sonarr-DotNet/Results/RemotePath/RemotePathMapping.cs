d772dcd7cd0398eee0ce536468691b0bd6aa730d
Mike Garvey <mike.garvey@yevrag35.com>
2019-08-14 10:29:31 -0400

    New Cmdlet: GetMapping
    RemotePathMapping

---
+++ MG.Sonarr-DotNet/Results/RemotePath/RemotePathMapping.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MG.Sonarr.Results
+{
+    public class RemotePathMapping : BaseResult
+    {
+        public string Host { get; set; }
+        [JsonProperty("id")]
+        public int MappingId { get; set; }
+        public string LocalPath { get; set; }
+        public string RemotePath { get; set; }
+    }
+}
