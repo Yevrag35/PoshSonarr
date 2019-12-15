21a24079922569929e2985c946c4680d628676f7
Mike Garvey <mike.garvey@yevrag35.com>
2019-12-15 12:05:07 -0500

    Wait for it

--- MG.Sonarr.Functionality/src/extensions/HttpClientExtensions.cs
+++ MG.Sonarr.Functionality/src/extensions/HttpClientExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MG.Sonarr.Functionality.Extensions
 {
@@ -9,5 +11,47 @@ namespace MG.Sonarr.Functionality.Extensions
     public static partial class HttpClientExtensions
     {
         private const string CONTENT_TYPE = "application/json";
+        public async static Task<IRestResponse<T>> GenerateSingleResponse<T>(HttpResponseMessage response, bool suppressExceptions = false)
+            where T : IJsonResult
+        {
+            var restResponse = new SonarrRestResponse<T>(response.StatusCode, response.IsSuccessStatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    restResponse.Result = await response.Content.ReadAsJsonAsync<T>(suppressExceptions).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    restResponse.Exception = e;
+                }
+            }
+            else
+            {
+                restResponse.Exception = new HttpRequestException(response.ReasonPhrase);
+            }
+            return restResponse;
+        }
+        public async static Task<IRestResponse<List<T>>> GenerateMultipleResponse<T>(HttpResponseMessage response, bool suppressExceptions = false)
+            where T : IJsonResult
+        {
+            var restResponse = new SonarrRestResponse<List<T>>(response.StatusCode, response.IsSuccessStatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    restResponse.Result = await response.Content.ReadAsJsonAsync<List<T>>(suppressExceptions).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    restResponse.Exception = e;
+                }
+            }
+            else
+            {
+                restResponse.Exception = new HttpRequestException(response.ReasonPhrase);
+            }
+            return restResponse;
+        }
     }
 }
