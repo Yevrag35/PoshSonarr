9f1fff32384013e628f2b280869194021eaf27e0
Mike Garvey <mike.garvey@yevrag35.com>
2020-01-12 18:07:18 -0500

    UpdateMetadata

--- MG.Sonarr-DotNet/Cmdlets/Metadata/GetMetadata.cs
+++ MG.Sonarr-DotNet/Cmdlets/Metadata/GetMetadata.cs
@@ -10,14 +10,8 @@ namespace MG.Sonarr.Cmdlets
     [Cmdlet(VerbsCommon.Get, "Metadata", ConfirmImpact = ConfirmImpact.None, DefaultParameterSetName = "None")]
     [OutputType(typeof(Metadata))]
     [CmdletBinding(PositionalBinding = false)]
-    public class GetMetadata : BaseSonarrCmdlet
+    public class GetMetadata : MetadataCmdlet
     {
-        #region FIELDS/CONSTANTS
-        private const string EP = "/metadata";
-        private const string EP_WITH_ID = EP + "/{0}";
-
-        #endregion
-
         #region PARAMETERS
         [Parameter(Mandatory = false, ParameterSetName = "MetadataById")]
         public int[] Id { get; set; }
@@ -34,31 +28,17 @@ namespace MG.Sonarr.Cmdlets
         protected override void ProcessRecord()
         {
             if (base.HasParameterSpecified(this, x => x.Id))
-                base.SendToPipeline(this.GetMetadataById(this.Id));
+                base.SendToPipeline(base.GetMetadataById(this.Id));
 
             else
             {
-                List<Metadata> all = base.SendSonarrListGet<Metadata>(EP);
+                List<Metadata> all = base.GetAllMetadata();
                 all.Sort();
                 if (base.HasParameterSpecified(this, x => x.Name))
                     base.SendToPipeline(base.FilterByStringParameter(all, m => m.Name, this, cmd => cmd.Name));
 
                 else
                     base.SendToPipeline(all);
-            }
-        }
-
-        #endregion
-
-        #region BACKEND METHODS
-        protected private List<Metadata> GetAllMetadata() => base.SendSonarrListGet<Metadata>(EP);
-        protected private IEnumerable<Metadata> GetMetadataById(int[] ids)
-        {
-            foreach (int id in ids)
-            {
-                Metadata maybe = base.SendSonarrGet<Metadata>(string.Format(EP_WITH_ID, id));
-                if (maybe != null)
-                    yield return maybe;
             }
         }
 
