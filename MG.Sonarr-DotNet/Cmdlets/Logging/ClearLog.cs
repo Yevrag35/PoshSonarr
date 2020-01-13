eeee086292b867790f0013e712a68c079585c684
Mike Garvey <mike.garvey@yevrag35.com>
2020-01-12 22:19:50 -0500

    ClearLog

---
+++ MG.Sonarr-DotNet/Cmdlets/Logging/ClearLog.cs
@@ -0,0 +1,44 @@
+﻿using MG.Sonarr.Functionality;
+using MG.Sonarr.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+
+namespace MG.Sonarr.Cmdlets.Logging
+{
+    [Cmdlet(VerbsCommon.Clear, "Log", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Low)]
+    [OutputType(typeof(CommandResult))]
+    [CmdletBinding]
+    public class ClearLog : BasePostCommandCmdlet
+    {
+
+        #region FIELDS/CONSTANTS
+        private const string CMD_STR = "clearlog";
+        private const string SP_MSG = "Issue a clear log command";
+        protected override string Command => CMD_STR;
+
+        #endregion
+
+        #region PARAMETERS
+
+
+        #endregion
+
+        #region CMDLET PROCESSING
+        protected override void BeginProcessing() => base.BeginProcessing();
+
+        protected override void ProcessRecord()
+        {
+            if (base.ShouldProcess(SP_MSG, "Clear"))
+                base.ProcessRequest(base.parameters);
+        }
+
+        #endregion
+
+        #region BACKEND METHODS
+
+
+        #endregion
+    }
+}
---
+++ MG.Sonarr-DotNet/Cmdlets/QualityProfiles/RemoveQualityProfile.cs
@@ -0,0 +1,69 @@
+﻿using MG.Sonarr.Functionality;
+using MG.Sonarr.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+
+namespace MG.Sonarr.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Remove, "QualityProfile", ConfirmImpact = ConfirmImpact.High, SupportsShouldProcess = true)]
+    [CmdletBinding]
+    public class RemoveQualityProfile : BaseSonarrCmdlet
+    {
+        #region FIELDS/CONSTANTS
+        private const string EP = "/profile";
+        private const string EP_WITH_ID = EP + "/{0}";
+
+        private bool _force;
+        private HashSet<int> _ids;
+
+        #endregion
+
+        #region PARAMETERS
+        [Parameter(Mandatory = true, ValueFromPipeline = true, DontShow = true, ParameterSetName = "ViaPipeline")]
+        public QualityProfile InputObject { get; set; }
+
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "ByProfileId")]
+        public int[] Id { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter Force
+        {
+            get => _force;
+            set => _force = value;
+        }
+
+        #endregion
+
+        #region CMDLET PROCESSING
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            if (base.HasParameterSpecified(this, x => x.Id))
+                _ids = new HashSet<int>(this.Id);
+
+            else
+                _ids = new HashSet<int>();
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (base.HasParameterSpecified(this, x => x.InputObject))
+                _ids.Add(this.InputObject.Id);
+        }
+
+        protected override void EndProcessing()
+        {
+            foreach (int id in _ids)
+            {
+                if (_force || base.FormatShouldProcess("Remove", "Quality Profile Id: {0}", id))
+                {
+                    base.SendSonarrDelete(string.Format(EP_WITH_ID, id));
+                }
+            }
+        }
+
+        #endregion
+    }
+}
