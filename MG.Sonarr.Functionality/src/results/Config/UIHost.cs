979528f93486bcb37bfb82c6c321d991383c795b
Mike Garvey <mike.garvey@yevrag35.com>
2020-02-17 19:04:51 -0500

    Minor Changes #6

--- MG.Sonarr.Functionality/src/results/Config/UIHost.cs
+++ MG.Sonarr.Functionality/src/results/Config/UIHost.cs
@@ -10,12 +10,18 @@ namespace MG.Sonarr.Results
     [JsonObject(MemberSerialization.OptIn)]
     public class UIHost : BaseResult
     {
+        [JsonExtensionData]
+        private IDictionary<string, object> _additional;
+
         #region JSON PROPERTIES
         [JsonProperty("id")]
-        public int UIHostId { get; private set; }
+        public int Id { get; private set; }
 
         [JsonProperty("analyticsEnabled", Order = 7)]
         public bool AnalyticsEnabled { get; set; }
+
+        [JsonProperty("apiKey", Order = 12)]
+        public IApiKey ApiKey { get; private set; }
 
         [JsonProperty("authenticationMethod", Order = 6)]
         [JsonConverter(typeof(SonarrStringEnumConverter))]
@@ -24,11 +30,8 @@ namespace MG.Sonarr.Results
         [JsonProperty("bindAddress", Order = 1)]
         public string BindAddress { get; set; }
 
-        [JsonProperty("port", Order = 2)]
-        public int Port { get; set; }
-
-        [JsonProperty("sslPort", Order = 3)]
-        public int SslPort { get; set; }
+        [JsonProperty("branch", Order = 11)]
+        public string Branch { get; set; }
 
         [JsonProperty("enableSsl", Order = 4)]
         public bool EnableSsl { get; set; }
@@ -36,27 +39,46 @@ namespace MG.Sonarr.Results
         [JsonProperty("launchBrowser", Order = 5)]
         public bool LaunchBrowser { get; set; }
 
-        [JsonProperty("username", Order = 8)]
-        public string Username { get; set; }
-
-        [JsonProperty("password", Order = 9)]
-        public string Password { get; set; }
-
         [JsonProperty("logLevel", Order = 10)]
         [JsonConverter(typeof(SonarrStringEnumConverter))]
         public LogLevel LogLevel { get; set; }
 
-        [JsonProperty("branch", Order = 11)]
-        public string Branch { get; set; }
+        [JsonProperty("password", Order = 9)]
+        public string Password { get; set; }
 
-        [JsonProperty("apiKey", Order = 12)]
-        public string ApiKey { get; private set; }
+        [JsonProperty("port", Order = 2)]
+        public int Port { get; set; }
+
+        [JsonProperty("proxyBypassFilter")]
+        public string ProxyBypassFilter { get; set; }
+
+        [JsonProperty("proxyBypassLocalAddresses")]
+        public bool ProxyBypassLocalAddresses { get; set; }
+
+        [JsonProperty("proxyEnabled")]
+        public bool ProxyEnabled { get; set; }
+
+        [JsonProperty("proxyHostname")]
+        public string ProxyHostname { get; set; }
+
+        [JsonProperty("proxyPassword")]
+        public string ProxyPassword { get; set; }
+
+        [JsonProperty("proxyPort")]
+        public int ProxyPort { get; set; }
+
+        [JsonProperty("proxyType")]
+        [JsonConverter(typeof(SonarrStringEnumConverter))]
+        public ProxyType ProxyType { get; set; }
+
+        [JsonProperty("proxyUserName")]
+        public string ProxyUsername { get; set; }
 
         [JsonProperty("sslCertHash", Order = 13)]
         public string SslCertHash { get; private set; }
 
-        [JsonProperty("urlBase", Order = 14)]
-        public string UrlBase { get; private set; }
+        [JsonProperty("sslPort", Order = 3)]
+        public int SslPort { get; set; }
 
         [JsonProperty("updateAutomatically", Order = 15)]
         public bool UpdateAutomatically { get; set; }
@@ -68,40 +90,23 @@ namespace MG.Sonarr.Results
         [JsonProperty("updateScriptPath", Order = 17)]
         public string UpdateScriptPath { get; set; }
 
-        [JsonProperty("proxyEnabled")]
-        public bool ProxyEnabled { get; set; }
+        [JsonProperty("urlBase", Order = 14)]
+        public string UrlBase { get; private set; }
 
-        [JsonProperty("proxyType")]
-        [JsonConverter(typeof(SonarrStringEnumConverter))]
-        public ProxyType ProxyType { get; set; }
+        [JsonProperty("username", Order = 8)]
+        public string Username { get; set; }
 
-        [JsonProperty("proxyHostname")]
-        public string ProxyHostname { get; set; }
+        //[JsonProperty("backupFolder")]
+        //public string BackupFolder { get; set; }
 
-        [JsonProperty("proxyPort")]
-        public int ProxyPort { get; set; }
+        //[JsonProperty("backupInterval")]
+        //public int BackupInterval { get; set; }
 
-        [JsonProperty("proxyUserName")]
-        public string ProxyUsername { get; set; }
-
-        [JsonProperty("proxyPassword")]
-        public string ProxyPassword { get; set; }
-
-        [JsonProperty("proxyBypassFilter")]
-        public string ProxyBypassFilter { get; set; }
-
-        [JsonProperty("proxyBypassLocalAddresses")]
-        public bool ProxyBypassLocalAddresses { get; set; }
-
-        [JsonProperty("backupFolder")]
-        public string BackupFolder { get; set; }
-
-        [JsonProperty("backupInterval")]
-        public int BackupInterval { get; set; }
-
-        [JsonProperty("backupRetention")]
-        public int BackupRetention { get; set; }
+        //[JsonProperty("backupRetention")]
+        //public int BackupRetention { get; set; }
 
         #endregion
+
+        public IDictionary<string, object> GetAdditionalData() => _additional;
     }
 }
