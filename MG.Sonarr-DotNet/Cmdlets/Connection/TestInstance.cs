39abfe561f34d65c96c614c6579be864a6c8d74e
Mike Garvey <mike.garvey@yevrag35.com>
2020-06-17 19:39:42 -0400

    Update references and TestInstance

---
+++ MG.Sonarr-DotNet/Cmdlets/Connection/TestInstance.cs
@@ -0,0 +1,24 @@
+﻿using MG.Sonarr.Functionality;
+using MG.Sonarr.Results;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace MG.Sonarr.Cmdlets
+{
+    [Cmdlet(VerbsDiagnostic.Test, "Instance")]
+    [Alias("Test-")]
+    public class TestInstance : BaseSonarrCmdlet
+    {
+
+
+        protected override void BeginProcessing() { }
+
+        protected override void ProcessRecord()
+        {
+
+        }
+
+        protected override void EndProcessing() { }
+    }
+}
