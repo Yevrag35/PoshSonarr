d0aad039cae6343e15b9114a438bd3aa812e9404
Mike Garvey <mike.garvey@yevrag35.com>
2020-08-01 16:56:56 -0400

    Change around parameters for Get-SonarrEpisode

--- MG.Sonarr-DotNet/Cmdlets/Episodes/GetEpisode.cs
+++ MG.Sonarr-DotNet/Cmdlets/Episodes/GetEpisode.cs
@@ -9,58 +9,65 @@ using System.Management.Automation;
 
 namespace MG.Sonarr.Cmdlets
 {
-    [Cmdlet(VerbsCommon.Get, "Episode", ConfirmImpact = ConfirmImpact.None, DefaultParameterSetName = "ByInputSeasonEp")]
+    [Cmdlet(VerbsCommon.Get, "Episode", ConfirmImpact = ConfirmImpact.None, DefaultParameterSetName = "NotId")]
     [CmdletBinding(PositionalBinding = false)]
     [OutputType(typeof(EpisodeResult))]
     public class GetEpisode : BaseSonarrCmdlet
     {
         #region FIELDS/CONSTANTS
-        private const string BASE = "/episode";
-        private const string EP_BY_SERIES = BASE + "?seriesId={0}";
-        private const string EP_BY_EP = BASE + "/{0}";
-        //private EpisodeIdentifierCollection _epIdCol;
         private EpisodeIdentifier _identifier;
 
         private bool _dled;
         private bool _hasAired;
+        private HashSet<long> _ids;
 
         #endregion
 
         #region PARAMETERS
-        [Parameter(Mandatory = true, DontShow = true, ValueFromPipeline = true, ParameterSetName = "ByInputAbsoluteEp")]
-        [Parameter(Mandatory = true, DontShow = true, ValueFromPipeline = true, ParameterSetName = "ByInputSeasonEp")]
-        public SeriesResult InputObject { get; set; }
+        //[Parameter(Mandatory = true, DontShow = true, ValueFromPipeline = true, ParameterSetName = "ByInputAbsoluteEp")]
+        //[Parameter(Mandatory = true, DontShow = true, ValueFromPipeline = true, ParameterSetName = "ByInputSeasonEp")]
+        [Parameter(Mandatory = false, ValueFromPipeline = true, ParameterSetName = "NotId")]
+        public SeriesResult[] InputObject { get; set; }
 
-        [Parameter(Mandatory = true, ParameterSetName = "BySeriesIdAbsoluteEp")]
-        [Parameter(Mandatory = true, ParameterSetName = "BySeriesIdSeasonEp")]
-        public int SeriesId { get; set; }
+        //[Parameter(Mandatory = true, ParameterSetName = "BySeriesIdAbsoluteEp")]
+        //[Parameter(Mandatory = true, ParameterSetName = "BySeriesIdSeasonEp")]
+        [Parameter(Mandatory = false, ParameterSetName = "NotId")]
+        public int[] SeriesId { get; set; }
 
         [Parameter(Mandatory = true, ParameterSetName = "ByEpisodeId")]
         [Alias("EpisodeId")]
-        public int Id { get; set; }
+        public long[] Id
+        {
+            get => _ids.ToArray();
+            set => _ids = new HashSet<long>(value);
+        }
 
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "BySeriesIdAbsoluteEp")]
-        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "ByInputAbsoluteEp")]
+        //[Parameter(Mandatory = true, ParameterSetName = "BySeriesIdAbsoluteEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "ByInputAbsoluteEp")]
+        [Parameter(Mandatory = false, ParameterSetName = "NotId")]
         public int[] AbsoluteEpisodeNumber { get; set; }
 
-        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "BySeriesIdSeasonEp")]
-        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "ByInputSeasonEp")]
+        //[Parameter(Mandatory = true, Position = 0, ParameterSetName = "BySeriesIdSeasonEp")]
+        //[Parameter(Mandatory = true, Position = 0, ParameterSetName = "ByInputSeasonEp")]
+        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "NotId")]
         public object[] EpisodeIdentifier { get; set; }
 
-        [Parameter(Mandatory = false, ParameterSetName = "ByInputAbsoluteEp")]
-        [Parameter(Mandatory = false, ParameterSetName = "ByInputSeasonEp")]
-        [Parameter(Mandatory = false, ParameterSetName = "BySeriesIdAbsoluteEp")]
-        [Parameter(Mandatory = false, ParameterSetName = "BySeriesIdSeasonEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "ByInputAbsoluteEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "ByInputSeasonEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "BySeriesIdAbsoluteEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "BySeriesIdSeasonEp")]
+        [Parameter(Mandatory = false, ParameterSetName = "NotId")]
         public SwitchParameter Downloaded
         {
             get => _dled;
             set => _dled = value;
         }
 
-        [Parameter(Mandatory = false, ParameterSetName = "ByInputAbsoluteEp")]
-        [Parameter(Mandatory = false, ParameterSetName = "ByInputSeasonEp")]
-        [Parameter(Mandatory = false, ParameterSetName = "BySeriesIdAbsoluteEp")]
-        [Parameter(Mandatory = false, ParameterSetName = "BySeriesIdSeasonEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "ByInputAbsoluteEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "ByInputSeasonEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "BySeriesIdAbsoluteEp")]
+        //[Parameter(Mandatory = false, ParameterSetName = "BySeriesIdSeasonEp")]
+        [Parameter(Mandatory = false, ParameterSetName = "NotId")]
         public SwitchParameter HasAired
         {
             get => _hasAired;
@@ -72,16 +79,28 @@ namespace MG.Sonarr.Cmdlets
         #region CMDLET PROCESSING
         protected override void BeginProcessing()
         {
+            base.BeginProcessing();
             if (this.ContainsParameter(x => x.EpisodeIdentifier))
             {
-                _identifier = Sonarr.EpisodeIdentifier.Parse(this.EpisodeIdentifier);
-                if (_identifier.AbsoluteSeasons.Count <= 0 && _identifier.AbsoluteEpisodes.Count <= 0 && _identifier.SeasonEpisodePairs.Count <= 0)
+                if (this.EpisodeIdentifier.Count(x => x is int || x is long) <= 0)
                 {
-                    base.ThrowTerminatingError(new ErrorRecord(new ArgumentException(
-                        "An invalid episode identifier was supplied, so no seasons or episodes were parsed."),
-                        typeof(ArgumentException).FullName,
-                        ErrorCategory.InvalidArgument,
-                        this.EpisodeIdentifier));
+                    _identifier = Sonarr.EpisodeIdentifier.Parse(this.EpisodeIdentifier);
+                    if (_identifier.AbsoluteSeasons.Count <= 0 && _identifier.AbsoluteEpisodes.Count <= 0 && _identifier.SeasonEpisodePairs.Count <= 0)
+                    {
+                        base.ThrowTerminatingError(new ErrorRecord(new ArgumentException(
+                            "An invalid episode identifier was supplied, so no seasons or episodes were parsed."),
+                            typeof(ArgumentException).FullName,
+                            ErrorCategory.InvalidArgument,
+                            this.EpisodeIdentifier));
+                    }
+                }
+                else
+                {
+                    _ids = new HashSet<long>(this.EpisodeIdentifier.OfType<long>());
+                    _ids.UnionWith(this.CastIdsToLong(this.EpisodeIdentifier.OfType<int>()));
+
+                    this.MyInvocation.BoundParameters.Add("Id", this.Id);
+                    this.MyInvocation.BoundParameters.Remove("EpisodeIdentifier");
                 }
             }
             else if (this.ContainsParameter(x => x.AbsoluteEpisodeNumber))
@@ -89,9 +108,6 @@ namespace MG.Sonarr.Cmdlets
                 _identifier = new Sonarr.EpisodeIdentifier();
                 _identifier.AbsoluteEpisodes.UnionWith(this.AbsoluteEpisodeNumber);
             }
-
-
-            base.BeginProcessing();
         }
 
         protected override void ProcessRecord()
@@ -99,16 +115,20 @@ namespace MG.Sonarr.Cmdlets
             var epList = new HashSet<EpisodeResult>();
             if (this.ContainsParameter(x => x.Id))
             {
-                this.GetEpisodeById(this.Id, epList);
+                this.GetEpisodeById(_ids, epList);
             }
             else
             {
                 if (this.ContainsParameter(x => x.InputObject))
-                    this.SeriesId = this.InputObject.Id;
+                    this.SeriesId = this.InputObject.Select(x => x.Id).ToArray();
 
-                List<EpisodeResult> allEps = base.SendSonarrListGet<EpisodeResult>(
-                    string.Format(ApiEndpoints.Episode_SeriesId, this.SeriesId)
-                );
+                List<EpisodeResult> allEps = new List<EpisodeResult>(24);
+                foreach (int sid in this.SeriesId)
+                {
+                    allEps.AddRange(base.SendSonarrListGet<EpisodeResult>(
+                        string.Format(ApiEndpoints.Episode_SeriesId, sid)
+                    ));
+                }
 
                 if (_identifier != null)
                 {
@@ -137,12 +157,22 @@ namespace MG.Sonarr.Cmdlets
         #endregion
 
         #region METHODS
-        private void GetEpisodeById(long epId, ISet<EpisodeResult> addToSet)
+        private IEnumerable<long> CastIdsToLong(IEnumerable<int> ints)
         {
-            string uri = string.Format(EP_BY_EP, epId);
-            EpisodeResult epRes = base.SendSonarrGet<EpisodeResult>(uri);
-            if (epRes != null)
-                addToSet.Add(epRes);
+            foreach (int i in ints)
+            {
+                yield return Convert.ToInt64(i);
+            }
+        }
+        private void GetEpisodeById(IEnumerable<long> epIds, ISet<EpisodeResult> addToSet)
+        {
+            foreach (long id in epIds)
+            {
+                string uri = string.Format(ApiEndpoints.Episode_ById, id);
+                EpisodeResult epRes = base.SendSonarrGet<EpisodeResult>(uri);
+                if (epRes != null)
+                    addToSet.Add(epRes);
+            }
         }
         private void GetEpisodeByIdentifier(EpisodeIdentifier identifier, IList<EpisodeResult> allEpisodes, ref HashSet<EpisodeResult> addToSet)
         {
