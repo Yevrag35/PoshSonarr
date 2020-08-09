b86222f2e62d956646838d1a54397c67c9fea101
Mike Garvey <mike.garvey@yevrag35.com>
2020-08-09 14:37:35 -0400

    Endpoint Struct

---
+++ MG.Sonarr.Functionality/Endpoints/Endpoint.cs
@@ -0,0 +1,37 @@
+﻿using MG.Sonarr.Functionality.Strings;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MG.Sonarr.Functionality
+{
+    public struct Endpoint
+    {
+        internal string Value;
+
+        private Endpoint(string apiEndpoint)
+        {
+            this.Value = apiEndpoint;
+        }
+
+        public static implicit operator string(Endpoint endpoint) => endpoint.Value;
+
+        public static Endpoint Backup => new Endpoint(ApiEndpoints.Backup);
+        
+    }
+
+    public static class EndpointExtensions
+    {
+        private const string SLASH_API = "/api";
+
+        public static Endpoint WithPrefix(this Endpoint endpoint) => WithPrefix(endpoint, true);
+        public static Endpoint WithPrefix(this Endpoint endpoint, bool addIt)
+        {
+            if (addIt)
+                endpoint.Value = SLASH_API + endpoint.Value;
+
+            return endpoint;
+        }
+    }
+}
