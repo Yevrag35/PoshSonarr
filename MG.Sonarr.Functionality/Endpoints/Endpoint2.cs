75e4f1a21114fab6282528eb935db816b1779f69
Mike Garvey <mike.garvey@yevrag35.com>
2020-08-15 09:38:37 -0400

    Ready to swap

--- MG.Sonarr.Functionality/Endpoints/Endpoint2.cs
+++ MG.Sonarr.Functionality/Endpoints/Endpoint2.cs
@@ -94,6 +94,41 @@ namespace MG.Sonarr.Functionality
             return new Endpoint2(this, newCol);
         }
 
+        #region STATIC CONSTRUCTORS
+        public static Endpoint2 Backup => new Endpoint2(ApiEndpoints.Backup);
+        public static Endpoint2 Calendar => new Endpoint2(ApiEndpoints.Calendar);
+        public static Endpoint2 Command => new Endpoint2(ApiEndpoints.Command);
+        public static Endpoint2 DelayProfile => new Endpoint2(ApiEndpoints.DelayProfile);
+        public static Endpoint2 Diskspace => new Endpoint2(ApiEndpoints.Diskspace);
+        public static Endpoint2 DownloadClient => new Endpoint2(ApiEndpoints.DownloadClient);
+        public static Endpoint2 Episode => new Endpoint2(ApiEndpoints.Episode);
+        public static Endpoint2 EpisodeFile => new Endpoint2(ApiEndpoints.EpisodeFile);
+        public static Endpoint2 FileSystem => new Endpoint2(ApiEndpoints.FileSystem);
+        public static Endpoint2 History => new Endpoint2(ApiEndpoints.History);
+        public static Endpoint2 HostConfig => new Endpoint2(ApiEndpoints.HostConfig);
+        public static Endpoint2 Indexer => new Endpoint2(ApiEndpoints.Indexer);
+        public static Endpoint2 IndexerOptions => new Endpoint2(ApiEndpoints.IndexerOptions);
+        public static Endpoint2 IndexerSchema => new Endpoint2(ApiEndpoints.IndexerSchema);
+        public static Endpoint2 LogFile => new Endpoint2(ApiEndpoints.LogFile);
+        public static Endpoint2 ManualImport => new Endpoint2(ApiEndpoints.ManualImport);
+        public static Endpoint2 MediaManagement => new Endpoint2(ApiEndpoints.MediaManagement);
+        public static Endpoint2 Metadata => new Endpoint2(ApiEndpoints.Metadata);
+        public static Endpoint2 Notification => new Endpoint2(ApiEndpoints.Notification);
+        public static Endpoint2 Profile => new Endpoint2(ApiEndpoints.Profile);
+        public static Endpoint2 QualityDefinition => new Endpoint2(ApiEndpoints.QualityDefinitions);
+        public static Endpoint2 Queue => new Endpoint2(ApiEndpoints.Queue);
+        public static Endpoint2 Release => new Endpoint2(ApiEndpoints.Release);
+        public static Endpoint2 RemotePathMapping => new Endpoint2(ApiEndpoints.Mapping);
+        public static Endpoint2 Restart => new Endpoint2(ApiEndpoints.Restart);
+        public static Endpoint2 Restriction => new Endpoint2(ApiEndpoints.Restriction);
+        public static Endpoint2 RootFolder => new Endpoint2(ApiEndpoints.RootFolder);
         public static Endpoint2 Series => new Endpoint2(ApiEndpoints.Series);
+        public static Endpoint2 Status => new Endpoint2(ApiEndpoints.Status);
+        public static Endpoint2 Tag => new Endpoint2(ApiEndpoints.Tag);
+        public static Endpoint2 Update => new Endpoint2(ApiEndpoints.Update);
+        public static Endpoint2 WantedMissing => new Endpoint2(ApiEndpoints.WantedMissing);
+
+
+        #endregion
     }
 }
