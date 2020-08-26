9ea40b316486d0d93b13b311f14f7144bca8b5e6
Mike Garvey <mike.garvey@yevrag35.com>
2020-08-26 00:00:20 -0400

    Use SortedSet for JsonStringSet

--- MG.Sonarr.Functionality/strings/JsonStringSet.cs
+++ MG.Sonarr.Functionality/strings/JsonStringSet.cs
@@ -5,17 +5,29 @@ using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MG.Sonarr.Functionality.Strings
 {
     [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
-    public class JsonStringSet : HashSet<string>, IJsonObject
+    public class JsonStringSet : SortedSet<string>, IReadOnlyList<string>, IJsonObject
     {
         private const string COMMA = ",";
         private static readonly string[] COMMA_ARR = new string[1] { COMMA };
 
-        public JsonStringSet() : base(SonarrFactory.NewIgnoreCase()) { }
-        public JsonStringSet(string singleString) : base(ConvertToStrings(singleString), SonarrFactory.NewIgnoreCase()) { }
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                    index = this.Count + index;
+
+                return this.ElementAtOrDefault(index);
+            }
+        }
+
+        public JsonStringSet() : base(SonarrFactory.NewIgnoreCaseComparer()) { }
+        public JsonStringSet(string singleString) : base(ConvertToStrings(singleString), SonarrFactory.NewIgnoreCaseComparer()) { }
 
         private static string[] ConvertToStrings(string oneStr)
         {
