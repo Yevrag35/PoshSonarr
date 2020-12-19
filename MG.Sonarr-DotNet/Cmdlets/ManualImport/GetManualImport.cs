4f80594a3ce18f0c8326bdfdfac7fe21ad68cecd
Mike Garvey <mike.garvey@yevrag35.com>
2020-12-19 13:39:38 -0500

    Apply ManualImport filtering logic
    Adds "SeriesName" and "NoRejections" to the parameter set.

--- MG.Sonarr-DotNet/Cmdlets/ManualImport/GetManualImport.cs
+++ MG.Sonarr-DotNet/Cmdlets/ManualImport/GetManualImport.cs
@@ -1,4 +1,5 @@
-﻿using MG.Sonarr.Functionality;
+﻿using MG.Posh.Extensions.Bound;
+using MG.Sonarr.Functionality;
 using MG.Sonarr.Results;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,8 @@ namespace MG.Sonarr.Cmdlets
     public class GetManualImport : BaseManualImportCmdlet
     {
         #region FIELDS/CONSTANTS
-
+        private List<ManualImport> _list;
+        private bool _noRejects;
 
         #endregion
 
@@ -21,23 +23,45 @@ namespace MG.Sonarr.Cmdlets
         [Alias("FullName")]
         public string[] Path { get; set; }
 
+        [Parameter(Mandatory = false, Position = 1)]
+        [SupportsWildcards]
+        public string[] SeriesName { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter NoRejections
+        {
+            get => _noRejects;
+            set => _noRejects = value;
+        }
+
         #endregion
 
         #region CMDLET PROCESSING
-        protected override void BeginProcessing() => base.BeginProcessing();
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            _list = new List<ManualImport>();
+        }
 
         protected override void ProcessRecord()
         {
             foreach (string importPath in this.Path)
             {
-                var list = base.GetPossibleImports(importPath);
-                base.SendToPipeline(list);
+                List<ManualImport> mis = base.GetPossibleImports(importPath);
+                if (mis != null && mis.Count > 0)
+                    _list.AddRange(base.FilterByStrings(mis, x => x.Series, this.SeriesName));
             }
         }
 
         protected override void EndProcessing()
         {
-
+            if (this.ContainsParameter(x => x.NoRejections))
+            {
+                base.SendToPipeline(_list.FindAll(
+                    x => (x.Rejections.Length <= 0) == _noRejects));
+            }
+            else
+                base.SendToPipeline(_list);
         }
 
         #endregion
