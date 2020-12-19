5b86dfdc8a155d6a7defa82a56ee5c04d7d8371c
Mike Garvey <mike.garvey@yevrag35.com>
2020-12-19 13:05:19 -0500

    Logs Sort By Ascending Time By Default
    Sonarr retrieves logs and reports them in Descending order.  This change places them in Ascending order into the collection instead.

--- MG.Sonarr.Functionality/results/Records/RecordCollection.cs
+++ MG.Sonarr.Functionality/results/Records/RecordCollection.cs
@@ -9,15 +9,9 @@ namespace MG.Sonarr.Results
     public class RecordCollection : ResultListBase<LogRecord>
     {
         [JsonConstructor]
-        internal RecordCollection(IEnumerable<LogRecord> records) : base(records) { }
-
-        public IList<ExceptionLogRecord> GetExceptionRecords()
+        internal RecordCollection(IEnumerable<LogRecord> records)
+            : base(records.OrderBy(x => x.Time))
         {
-            return base.InnerList.OfType<ExceptionLogRecord>().ToList();
-        }
-        public IList<LogRecord> GetNonExceptionRecords()
-        {
-            return base.InnerList.Where(x => !(x is ExceptionLogRecord)).ToList();
         }
     }
 }
