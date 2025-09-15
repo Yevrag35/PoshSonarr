7f961c56f95d1a6f9a6562c634baaab09902dcb8
Mike Garvey <mike.garvey@yevrag35.com>
2025-09-15 16:04:54 -0400

    Refactor query fields and remove legacy PagingParameter
    Refactored `FormattableQueryField` to replace `Either` with a new `State` struct for improved internal representation and performance. Removed the legacy `PagingParameterOld.cs` file and its references from the project. Simplified `KeyOnlyQueryField` by removing `_isNotEmpty` and updating `IsDefaultOrEmpty` logic.
    
    Introduced `UnsafeEither<T1, T2>` as a lightweight alternative to `Either`. Removed usages of `MG.Sonarr.Next.Extensions.Strings` and `MG.Sonarr.Next.Extensions`. Added package references for `Microsoft.Extensions.Caching.Memory` and `Microsoft.Extensions.DependencyInjection` for `net8.0`.
    
    Performed general code cleanup, including removal of unused fields, methods, and comments, to improve maintainability.

---
+++ src/MG.Sonarr.Next/Unions/UnsafeEither.cs
@@ -0,0 +1,19 @@
+﻿namespace MG.Sonarr.Next.Unions
+{
+    internal readonly struct UnsafeEither<T1, T2> where T1 : class where T2 : class
+    {
+        private readonly object? _value;
+        internal object? Value => _value;
+
+        internal UnsafeEither(T1 value)
+        {
+            _value = value;
+        }
+        internal UnsafeEither(T2 value)
+        {
+            _value = value;
+        }
+
+
+    }
+}
